17f5c9fae22badd3d24dd2885a39d7e4c76536f4
john <john.d.leonetti@gmail.com>
2021-03-03 10:07:30 -0800

    commit tree 8fdc93129368128dd6e746afcf749c05e7638262

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,60 @@
 # code here!
+require "pry"
+class School
+
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end
+    attr_accessor :roster
+   
+    def add_student(student, grade)
+        if @roster[grade]
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+        
+
+    def grade(grade)
+       
+         @roster[grade]
+         
+    end
+    
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+        
+        end
+        return @roster
+
+    end
+   
+    # def grade(year)
+    #     @year = year
+
+    # end
+
+end   
+      
+    
+       
+
+        
+    
+
+        
+
+
+
+
+
+
+
+# students.each do |name, grade|
+#     @roster = ""
+
+#     roster = {student_name:[], student_grade:[] }
